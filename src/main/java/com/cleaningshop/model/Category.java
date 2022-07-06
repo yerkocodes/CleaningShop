8ad29372018dd49c96aa5c10da68a84728acea68
Yerko Cortes Baeza <yerkodigo@gmail.com>
2022-07-06 10:03:02 -0400

    Category model class added

---
+++ src/main/java/com/cleaningshop/model/Category.java
@@ -0,0 +1,31 @@
+package com.cleaningshop.model;
+
+public class Category {
+	private int id_category;
+	private String name_category;
+	
+	public Category() {}
+
+	public Category(int id_category, String name_category) {
+		super();
+		this.id_category = id_category;
+		this.name_category = name_category;
+	}
+
+	public int getId_category() {
+		return id_category;
+	}
+
+	public void setId_category(int id_category) {
+		this.id_category = id_category;
+	}
+
+	public String getName_category() {
+		return name_category;
+	}
+
+	public void setName_category(String name_category) {
+		this.name_category = name_category;
+	}
+	
+}
