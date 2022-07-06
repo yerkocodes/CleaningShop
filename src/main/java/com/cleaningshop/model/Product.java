10f6f6f6933d0c9f27b1a71f6be46d6c00bd50a1
Yerko Cortes Baeza <yerkodigo@gmail.com>
2022-07-06 09:33:09 -0400

    Product model class added

---
+++ src/main/java/com/cleaningshop/model/Product.java
@@ -0,0 +1,64 @@
+package com.cleaningshop.model;
+
+public class Product {
+
+	private int id_product;
+	private String name_product;
+	private int price_product;
+	private String description_product;
+	private int id_category;
+	
+	public Product() {}
+
+	public Product(int id_product, String name_product, int price_product, String description_product,
+			int id_category) {
+		super();
+		this.id_product = id_product;
+		this.name_product = name_product;
+		this.price_product = price_product;
+		this.description_product = description_product;
+		this.id_category = id_category;
+	}
+
+	public int getId_product() {
+		return id_product;
+	}
+
+	public void setId_product(int id_product) {
+		this.id_product = id_product;
+	}
+
+	public String getName_product() {
+		return name_product;
+	}
+
+	public void setName_product(String name_product) {
+		this.name_product = name_product;
+	}
+
+	public int getPrice_product() {
+		return price_product;
+	}
+
+	public void setPrice_product(int price_product) {
+		this.price_product = price_product;
+	}
+
+	public String getDescription_product() {
+		return description_product;
+	}
+
+	public void setDescription_product(String description_product) {
+		this.description_product = description_product;
+	}
+
+	public int getId_category() {
+		return id_category;
+	}
+
+	public void setId_category(int id_category) {
+		this.id_category = id_category;
+	}
+	
+	
+}
